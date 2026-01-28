1fb8408b1b61949c08acb74dfc057f5cd3cea3de
MichaelAhliIT <michaelcw93@gmail.com>
2026-01-28 20:49:12 +0800

    change footer and sidebar

--- src/app/[locale]/components/Footer.tsx
+++ src/app/[locale]/components/Footer.tsx
@@ -4,9 +4,9 @@ export const Footer = () => {
   const t = useTranslations("Footer");
 
   return (
-    <footer className="footer sm:footer-horizontal text-black p-10 bg-slate-100">
+    <footer className="footer sm:footer-horizontal text-gray-200 p-10 bg-red-700">
       <nav>
-        <h6 className="footer-title">{t("products")}</h6>
+        <h6 className="footer-title text-white opacity-100">{t("products")}</h6>
         <a className="link link-hover">{t("detergent")}</a>
         <a className="link link-hover">{t("dishwashing")}</a>
         <a className="link link-hover">{t("floor")}</a>
@@ -16,28 +16,23 @@ export const Footer = () => {
         <a className="link link-hover">{t("cotton")}</a>
       </nav>
       <nav>
-        <h6 className="footer-title">{t("company")}</h6>
+        <h6 className="footer-title text-white opacity-100">{t("company")}</h6>
         <a className="link link-hover">{t("about")}</a>
         <a className="link link-hover">{t("contact")}</a>
         <a className="link link-hover">{t("faq")}</a>
         <a className="link link-hover">{t("toc")}</a>
       </nav>
       <nav>
-        <h6 className="footer-title">{t("social")}</h6>
+        <h6 className="footer-title text-white opacity-100">{t("social")}</h6>
         <div className="grid grid-flow-col gap-4">
           <a>
-            <img
-              width="28"
-              height="28"
-              src="https://img.icons8.com/color/48/shopee.png"
-              alt="shopee"
-            />
+            <img width="28" height="28" src="/shopee_white.png" alt="shopee" />
           </a>
           <a>
             <img
               width="28"
               height="28"
-              src="https://img.icons8.com/ios-filled/48/tiktok--v1.png"
+              src="/tiktok_white.png"
               alt="tiktok--v1"
             />
           </a>
@@ -45,7 +40,7 @@ export const Footer = () => {
             <img
               width="28"
               height="28"
-              src="https://img.icons8.com/fluency/48/instagram-new.png"
+              src="/instagram_white.png"
               alt="instagram-new"
             />
           </a>
--- src/app/[locale]/components/PopupSidebar.tsx
+++ src/app/[locale]/components/PopupSidebar.tsx
@@ -34,6 +34,11 @@ export default function PopupSidebar({
     cotton: t("cotton"),
   };
 
+  const allCategories = {
+    ...luckyCleanCategory,
+    ...luckyCareCategory,
+  };
+
   return (
     <div className="relative flex flex-1 md:hidden">
       {/* Hamburger Menu Button */}
@@ -50,22 +55,22 @@ export default function PopupSidebar({
       </button>
 
       {/* Overlay */}
-      {isOpen && (
+      {/* {isOpen && (
         <div
           className="fixed inset-0 bg-black/40 bg-opacity-50 z-20"
           onClick={toggleSidebar}
         />
-      )}
+      )} */}
 
       {/* Sidebar */}
       <div
-        className={`fixed top-0 left-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-30 ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
+        className={`fixed top-0 left-0 h-full w-full bg-white shadow-lg transform transition-transform duration-1000 ease-in-out z-30 ${
+          isOpen ? "translate-y-0" : "-translate-y-full"
         }`}
       >
         <div className="p-5">
           <div className="flex justify-between items-center mb-6">
-            <h2 className="text-xl font-bold text-gray-800">{t("products")}</h2>
+            <h2 className="text-2xl text-red-800">{t("products")}</h2>
             <button
               onClick={toggleSidebar}
               className="p-1 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none"
@@ -76,22 +81,24 @@ export default function PopupSidebar({
 
           <nav>
             <ul className="space-y-2">
-              <li className="bg-[#BF0004] p-3">
+              {/* <li className="bg-[#BF0004] p-3">
                 <h1 className="text-white">Lucky Clean</h1>
-              </li>
-              {Object.entries(luckyCleanCategory).map(([key, label]) => (
+              </li> */}
+              {Object.entries(allCategories).map(([key, label]) => (
                 <li key={key}>
                   <button
                     onClick={() => handleSelect(key)}
-                    className={`block w-full text-left p-2 rounded-md hover:bg-gray-200 ${
-                      category === key ? "bg-gray-200 font-bold" : ""
+                    className={`block w-full p-2 text-left rounded-md text-xl font-light ${
+                      category === key
+                        ? "text-red-700 font-bold underline underline-offset-8"
+                        : ""
                     }`}
                   >
                     {label}
                   </button>
                 </li>
               ))}
-              <li className="bg-[#BF0004] p-3">
+              {/* <li className="bg-[#BF0004] p-3">
                 <h1 className="text-white">Lucky Care</h1>
               </li>
               {Object.entries(luckyCareCategory).map(([key, label]) => (
@@ -105,7 +112,7 @@ export default function PopupSidebar({
                     {label}
                   </button>
                 </li>
-              ))}
+              ))} */}
             </ul>
           </nav>
         </div>
